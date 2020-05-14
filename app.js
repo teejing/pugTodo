a0b74b8b5514bb40d99f1d106dad69f186b599b7
teejing <patricksemail@tutanota.com>
2020-05-14 21:58:30 +0200

    initial Express

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.set('view engine', 'pug')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.render('index', { title: 'Hey', message: 'Hello there!' }))
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
